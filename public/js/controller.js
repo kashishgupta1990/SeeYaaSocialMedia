567555de103d09ab387fa508b22d57114d1436a5
Kashish Gupta <kashishgupta1990@yahoo.com>
2014-07-15 16:10:57 +0530

    app Merg

--- public/js/controller.js
+++ public/js/controller.js
@@ -4,6 +4,10 @@ myApp.config(function ($routeProvider) {
     $routeProvider
 
         .when('/', {
+            templateUrl: 'view/login.html',
+            controller: 'loginController'
+        })
+        .when('/home', {
             templateUrl: 'view/home.html',
             controller: 'homeController'
         })
@@ -16,6 +20,49 @@ myApp.config(function ($routeProvider) {
             controller: 'formController'
         })
 });
+
+//Login Controller
+myApp.controller('loginController', ['$scope', '$http', function ($scope, $http) {
+    console.log('i m in Login Controller');
+
+    $scope.signupClick = function () {
+        console.log('signUp Click');
+        console.log($scope.emailId);
+        var tmpObj = {email: $scope.emailId};
+        $http.post("/signuprequest", tmpObj)
+            .success(function (data) {
+                console.log(data);
+            })
+            .error(function (err) {
+                console.log(err);
+            });
+
+    }
+
+    $scope.logInClick = function () {
+        console.log('LogIn Button Clicked');
+
+        console.log($scope.email);
+        console.log($scope.password);
+
+        $http.post('/verifyAccount', {email: $scope.email, password: $scope.password})
+            .success(function (result) {
+                console.log(result);
+
+                if (result != "Invalid User") {
+                    console.log('Successfully Login');
+                    //$location.path('master.html');
+                }
+                else {
+                    alert('Invalid User');
+                }
+
+            })
+            .error(function (err) {
+                console.log(err);
+            })
+    }
+}]);
 
 //Main Controller
 myApp.controller('mainController', ['$scope', '$http', function ($scope, $http) {
